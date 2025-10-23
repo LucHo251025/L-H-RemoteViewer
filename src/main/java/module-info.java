cdc6ed13a1994487ae3de776822bd6359c027832
MHuyen <huyenhtm.23itb@vku.udn.vn>
2025-10-23 20:26:01 +0700

    commit code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,22 @@
-module com.example.lhremoteviewer {
+module com.example.ultraviewdemo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires javafx.graphics;
+    requires org.json;
 
 
-    opens com.example.lhremoteviewer to javafx.fxml;
-    exports com.example.lhremoteviewer;
+    exports com.example.ultraviewdemo.client;
+    opens com.example.ultraviewdemo.client to javafx.fxml;
+    opens com.example.ultraviewdemo to javafx.fxml;
+    exports com.example.ultraviewdemo;
 }
