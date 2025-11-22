9ac24f113092e102cb132409eb293aad83695d1b
MHyunne <hah25703@gmail.com>
2025-11-22 16:58:21 +0700

    commit code

--- src/main/java/com/example/ultraviewdemo/client/ViewerClient.java
+++ src/main/java/com/example/ultraviewdemo/client/ViewerClient.java
@@ -172,6 +172,13 @@ public class ViewerClient extends Application {
         controlStage.setScene(scene);
         controlStage.setMinWidth(1000);
         controlStage.setMinHeight(700);
+
+        javafx.scene.control.Button disconnectBtn =
+                (javafx.scene.control.Button) scene.lookup("#disconnectBtn");
+        if (disconnectBtn != null) {
+            disconnectBtn.setOnAction(e -> controlStage.close());
+        }
+
         controlStage.show();
     }
 
